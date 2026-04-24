ec51463cc29b1548aa8f463026d24b8a3fbce6bd
Jannik Brozy <jannik.brozy@student.fh-kiel.de>
2026-04-24 08:30:30 +0200

    remove backup

--- cast.core.b.ak/grammar/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! command -v antlr >/dev/null 2>&1
-then
-    echo "executable for antlr could not be found."
-    exit 1
-fi
-
-antlr -Dlanguage=CSharp -visitor -package Cast.Compiler CastLexer.g4 CastParser.g4 CastPreParser.g4
